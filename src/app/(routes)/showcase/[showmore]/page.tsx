ef9a9481abbae56a8788d906599dd58af3d194df
Manasi-007 <Manasi.Behera@iseyon.com>
2024-08-23 16:15:53 +0530

    client based authorization is done

--- src/app/(routes)/showcase/[showmore]/page.tsx
+++ src/app/(routes)/showcase/[showmore]/page.tsx
@@ -9,38 +9,46 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const id = params.showmore;
+
+  const clientApp = ['0' , '1' , '2'];
   // let usertype;
   // if(id == '0' || id == '1' || id =='2' ){
   //    usertype = 'client';
   // }
-  // console.log("id= ",id);  
-  const rawData = await fetch('http://localhost:8080/news');
-  
+  // console.log("id= ",id);
+  const rawData = await fetch("http://localhost:8080/news");
+
   if (!rawData.ok) {
-    throw new Error('Failed to fetch the data!!!');
+    throw new Error("Failed to fetch the data!!!");
   }
 
   const data = await rawData.json();
-  
-  const supabase = createClient()
-    const { data: activeSession } = await supabase.auth.getSession();
 
-	if (!activeSession.session) {
-		return redirect("/");
-	}
+  const supabase = createClient();
+  const { data: activeSession } = await supabase.auth.getSession();
 
-	const { data: user } = await supabase.from("user").select("*").single();
+  if (!activeSession.session) {
+    return redirect("/");
+  }
 
+  const { data: user } = await supabase.from("user").select("*").single();
 
   if (user?.role === "user") {
-    return(
-       <h1 className='h-screen flex justify-center items-center'>Sorry You Don't have access to this page</h1>
-      
-    )
-	}
-
+    return (
+      <h1 className="h-screen flex justify-center items-center">
+        Sorry You Don't have access to this page
+      </h1>
+    );
+  }
+  else if(user?.role === "client" && !clientApp.includes(id)){
+    return (
+      <h1 className="h-screen flex justify-center items-center">
+        Sorry You Don't have access to this page
+      </h1>  
+    );
+  } 
   return (
     <ViewMorePage id={id} news={data} />
     // <pre className="text-white mt-60 ml-80">{JSON.stringify(user,null,2)}</pre>
   );
-}
+} 

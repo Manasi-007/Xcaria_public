aeae2ca985bbd9e1b4915d2628c1dfa3da0b515e
Manasi-007 <Manasi.Behera@iseyon.com>
2024-09-02 15:53:51 +0530

    error resolved

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,15 +1,15 @@
 
 
 export function Analytics() {
-	// const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-	// console.log(token)
-	// if (!token) {
-	// 	return null;
-	// }
+	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
+	console.log(token)
+	if (!token) {
+		return null;
+	}
 	return (
 		<script
 			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			// data-token={token}
+			data-token={token}
 			async
 		/>
 	);
